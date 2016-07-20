95c973a0b1fcf158024b3ffd08f6b0ab25b114ef
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-20 11:34:53 -0500

    Use destructuring to find page param

--- app/routes/near-earth.js
+++ app/routes/near-earth.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Route.extend({
-  model(params) {
-    const page = params.page;
+  model({ page }) {
+    // const page = arguments[0].page;
+    // const { page } = arguments[0];
 
     return fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${config.nasaKey}&page=${page}`)
       .then((res) => res.json());
